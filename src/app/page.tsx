e5a77a435baee85df9dbb97041e34285665853a7
Brandocean <brandocean2016@gmail.com>
2024-03-04 19:28:56 -0600

    Search users

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,12 @@
-import LoginButton from "./components/LoginButton"
-import { GoogleOAuthProvider } from '@react-oauth/google';
-
+import LoginButton from "./components/LoginButton";
+import { GoogleOAuthProvider } from "@react-oauth/google";
 
 export default function Home() {
   return (
-
-    <GoogleOAuthProvider clientId='722295534456-g5lg8gbkmok7g4lmb5lp0rrdrbshgjfh.apps.googleusercontent.com'>
+    <GoogleOAuthProvider clientId="722295534456-g5lg8gbkmok7g4lmb5lp0rrdrbshgjfh.apps.googleusercontent.com">
       <main>
         <LoginButton />
       </main>
     </GoogleOAuthProvider>
-    
-  )
+  );
 }
