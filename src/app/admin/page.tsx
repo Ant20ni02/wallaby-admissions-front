7228da895f2c6717b92e675980e9852ff5dceccd
Brandocean <brandocean2016@gmail.com>
2024-03-04 21:17:25 -0600

    Add Sun Loader

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,14 +1,17 @@
 "use client";
 import axios from "axios";
 import { useState } from "react";
+import Loading from "../components/SunLoader";
 
 export default function AdminHome() {
   const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(false); // Estado para manejar la carga
 
   console.log(data);
 
-  const getInfo: (email: string) => void = async function (email: string) {
-    await axios
+  const getInfo: (email: string) => void = function (email: string) {
+    setLoading(true); // Establecer el estado de carga a verdadero
+    axios
       .get(`/api/getRowByEmail/${email}`, {
         headers: {
           Accept: "application/json",
@@ -16,18 +19,22 @@ export default function AdminHome() {
       })
       .then((res) => {
         setData(res.data.row);
+        setLoading(false); // Establecer el estado de carga a falso cuando se recibe la respuesta
       })
-      .catch((err) => console.log(err));
+      .catch((err) => {
+        console.log(err);
+        setLoading(false); // Establecer el estado de carga a falso en caso de error también
+      });
   };
 
   const obtain: (
     event: React.MouseEvent<HTMLButtonElement, MouseEvent>
-  ) => void = async function (
+  ) => void = function (
     event: React.MouseEvent<HTMLButtonElement, MouseEvent>
   ) {
     event.preventDefault(); // Evitar que la página se recargue
     var email = (document.getElementById("test") as HTMLInputElement).value;
-    await getInfo(email);
+    getInfo(email);
   };
 
   return (
@@ -49,16 +56,26 @@ export default function AdminHome() {
         </p>
       </div>
 
-      <div>
-        <h4>Nombre del papá</h4>
-        <p>{data.length > 0 ? data[13] : "Diego Delgado"}</p>
-        
-        <h4>Nombre del hijo (a)</h4>
-        <p>{data.length > 0 ? data[1]+" "+data[2]+" "+data[3] : "Diego Delgado segundo"}</p>
+      {/* Mostrar el componente de carga si el estado de carga es verdadero */}
+      {loading && <Loading />}
 
-        <h4>Correo electrónico</h4>
-        <p>{data.length > 0 ? data[12] : "diego@gmail.com"}</p>
-      </div>
+      {/* Mostrar los datos si no hay carga */}
+      {!loading && (
+        <div>
+          <h4>Nombre del papá</h4>
+          <p>{data.length > 0 ? data[13] : "Diego Delgado"}</p>
+
+          <h4>Nombre del hijo (a)</h4>
+          <p>
+            {data.length > 0
+              ? data[1] + " " + data[2] + " " + data[3]
+              : "Diego Delgado segundo"}
+          </p>
+
+          <h4>Correo electrónico</h4>
+          <p>{data.length > 0 ? data[12] : "diego@gmail.com"}</p>
+        </div>
+      )}
     </>
   );
 }
