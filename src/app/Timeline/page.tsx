2e8cdc8aad976d1f142af53695eb436329fbf16c
Miguel Jiménez <mike0vortex@gmail.com>
2024-03-06 16:44:17 -0600

    Timeline page updated

--- src/app/Timeline/page.tsx
+++ src/app/Timeline/page.tsx
@@ -1,32 +1,24 @@
 "use client";
 import page from "../Timeline/timeline.module.css"
 import StateComponent from "../components/StateComponent";
-import { useRouter } from 'next/navigation';
 import { nodeProperties } from '../types';
-import { AppRouterInstance } from "next/dist/shared/lib/app-router-context.shared-runtime";
 
-import schoolState1 from '../../../public/school-state1.png';
+import schoolState1 from '/public/school-state1.png';
 import sunState2 from '../../../public/sun-state2.png';
 import fileState3 from '../../../public/files-state3.png';
 import cardState4 from '../../../public/cards-state4.png';
 import backpackStage5 from '../../../public/backpack-state5.png';
 import formStage6 from '../../../public/form-stage6.png';
-import { useEffect, useLayoutEffect, useReducer, useState } from "react";
+import { useEffect, useReducer, useState } from "react";
 import { StaticImageData } from "next/image";
 
 const Timeline = ({ }) => {
 
-    const router : AppRouterInstance = useRouter();
     const [properties, setProperties] = useState<Array<nodeProperties>>([]);
-    //const [currentStatus, setCurrentStatus] = useState<string>("");
     const [, forceUpdate] = useReducer(x => x + 1, 0);
-
-    
     const [nodeImages, setNodeImages] = useState<Array<StaticImageData>>([schoolState1, sunState2, fileState3, cardState4, backpackStage5, formStage6]);
 
     let dynamicProp: nodeProperties = { index: 0, color: "", imgSrc: schoolState1 }
-    //const [decoyValue, setDecoyValue] = useState<number>(0);
-
 
     let color = "";
 
@@ -57,28 +49,26 @@ const Timeline = ({ }) => {
                 break;
         }
 
-        if(currentStatus === "DIA_PRUEBA"){
-            console.log("kiti1");
-        } else{
-            console.log("kiti2")
-        }
-
 
         color = "";
         console.log(currentStatus);
         console.log(decoyValue);
 
-        for (let x = 2; x <= 8; x++){
+        for (let x = 2; x <= 7; x++){
             //dynamicProp = {index : x, color : }
-            if(x <= decoyValue){
-                color = "#39B54A";
-            }
-            else if(x === decoyValue+1){
-                color = "#22629E";
-            }
+
+            if (x === decoyValue) { color = "#22629E";}
             else{
-                color = "#FFFFFF";
+                if (x <= decoyValue) {
+                    color = "#39B54A";
+                }
+
+                else {
+                    color = "#FFFFFF";
+                }
             }
+
+            
 
             dynamicProp = { index: x, color: color, imgSrc: nodeImages[x-2] };
 
@@ -97,14 +87,11 @@ const Timeline = ({ }) => {
 
     return (
         <>
-            <div className={page.topBand}></div>
 
             <div className={page.generalItemsWrap}>
                 <div className={page.headerItemsWrap}>
-
-                    <span className={page.headerText}> ¡Bienvenidos al proceso de admisión de      </span>
                     <img src="https://wallaby.edu.mx/wp-content/uploads/thegem-logos/logo_4c4b74d94dc18e7b988f3224ed408701_2x.png" alt="wallabyLogo" width="120em" />
-                    <span className={page.headerText}>!</span>
+                    <span className={page.headerText}> ¡Aquí inicia tu proceso de admisión!</span>
 
                 </div>
 
@@ -121,9 +108,6 @@ const Timeline = ({ }) => {
                 </div>
 
             </div>
-
-            <div className={page.bottomBand}></div>
-
 
         </>
     )
